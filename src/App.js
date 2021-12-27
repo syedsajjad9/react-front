9fea9254309d107ff244976ee29b13bb2c1a7498
Syed Sajjad <syedabdulsajjad9@gmail.com>
2021-12-27 14:37:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import AllUsers from './Components/AllUsers';
+import AddUser from './Components/AddUser';
+import EditUser from './Components/EditUser';
+import NavBar from './Components/NavBar';
+import NotFound from './Components/NotFound'; 
+// import CodeForInterview from './Components/CodeForInterview';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        {/* <Route exact path="/" component={CodeForInterview} /> */}
+        <Route exact path="/all" component={AllUsers} />
+        <Route exact path="/add" component={AddUser} />
+        <Route exact path="/edit/:id" component={EditUser} />
+        <Route component={NotFound} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Service/api.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+// const usersUrl = 'http://localhost:9000/users';
+const usersUrl = 'https://demo-curd-sajjad.herokuapp.com/users';
+
+
+
+export const getUsers = async(id) => {
+    id = id || '';
+    return await axios.get(`${usersUrl}/${id}`);
+}
+
+export const addUser = async(user) => {
+    return await axios.post(`${usersUrl}/add`, user);
+}
+
+export const deleteUser = async(id) => {
+    return await axios.delete(`${usersUrl}/${id}`);
+}
+
+export const editUser = async(id, user) => {
+    return await axios.put(`${usersUrl}/${id}`, user)
+}
